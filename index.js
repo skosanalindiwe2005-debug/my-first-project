496eaf967f53a009efa4e0fd1e9f89c60f409c6d
skosanalindiwe2005-debug <skosanalindiwe2005@gmail.com>
2025-11-16 14:56:02 +0200

    Updated my Weather app and added new CSS properties

--- index.js
+++ index.js
@@ -105,3 +105,15 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+
+function changeTheme() {
+  let body = document.querySelector("body");
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+  } else {
+    body.classList.add("dark");
+  }
+}
+
+let themeButton = document.querySelector(".theme-button");
+themeButton.addEventListener("click", changeTheme);
